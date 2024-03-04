6e78a2f00389e7b701536135e7c3f4316b3aa49d
Andguzman <105981706+Andguzman@users.noreply.github.com>
2024-03-03 23:32:26 -0600

    And
    Se agrega la operacion and de N bits utilizando el operador, con su respectivo testbench para 8 bits

---
+++ problem1/andoperation_tb.sv
@@ -0,0 +1,31 @@
+module andoperation_tb;
+
+    // Parameters
+    parameter N = 8;
+    
+    // Signals
+    logic [N-1:0] a, b;
+    logic [N-1:0] result;
+    
+    // Instantiate the module
+    andoperation #(N) andmodule (
+        .a(a),
+        .b(b),
+        .result(result)
+    );
+
+    // Test stimulus
+    initial begin
+        // Initialize inputs
+        a = 8'b10010110; // Example value for a
+        b = 8'b10101010; // Example value for b
+        
+        // Apply stimulus
+        #20;
+        $display("Result = %h", result);
+        
+        // End simulation
+        $finish;
+    end
+
+endmodule
