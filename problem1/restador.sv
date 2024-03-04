4daf53e672f1c1581c68e4c36681f768173c40de
Andguzman <105981706+Andguzman@users.noreply.github.com>
2024-03-03 21:59:30 -0600

    OperacionesBasicas
    Se pueden realizar 3 operaciones aritmeticas

---
+++ problem1/restador.sv
@@ -0,0 +1,38 @@
+
+module restador #(parameter BITS= 4) (input logic [BITS-1:0] num1, num2, output logic
+Carryout, output logic [BITS-1:0] Result);
+		logic [BITS-1:0] int_res;
+		logic [BITS-1:0] int_resO;
+		logic [BITS-1:0] int_resI;
+		logic [BITS-1:0] num2C;
+    // Se convierte en complemento 1 el numero negativo
+    inversor #(BITS) inverter (
+        .in(num2),
+        .out(num2C)
+    );
+	 
+	 sumador #(BITS) sumador1(.num1(num1), 
+	 .num2(num2C), .Cout(Carryout), 
+	 .Resul(int_res)
+	 );
+	 
+	 always_comb begin
+		if (Carryout == 1) begin
+			int_resO = 'b0;
+			Result = int_res;
+		end
+		else begin
+			int_resO = int_res;
+			Result = int_resI;
+			end
+		end
+	 inversor #(BITS) inverter2 (
+			  .in(int_resO),
+			  .out(int_resI)
+		 );
+
+endmodule
+
+
+
+
---
+++ problem1/restador_tb.sv
@@ -0,0 +1,33 @@
+
+module restador_tb;
+
+    // Parameters
+    parameter N = 4;
+
+    // Signals
+    logic [N-1:0] in_signal;
+	 logic [N-1:0] in_signal2;
+    logic [N-1:0] out_signal;
+	 logic carryout;
+
+    // Instantiate the inverter module
+    restador #(N) myrestador (
+        .num1(in_signal),
+		  .num2(in_signal2),
+		  .Carryout(carryout),
+        .Result(out_signal)
+    );
+
+    // Stimulus
+    initial begin
+        // Test case 3: Random input
+        in_signal = 4'b1010;
+        in_signal2 = 4'b0001;
+        #10; // Wait for 10 time units
+        // Expected output: Inverted input
+        $display("Input1: %b, Input2: %b, Output: %b, Carry %b", in_signal,in_signal2, out_signal, carryout);
+        // Terminate the simulation
+        $finish;
+    end
+
+endmodule
